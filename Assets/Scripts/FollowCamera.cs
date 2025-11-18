09f471c63e2dade26c9564240c1519a54117fab9
Haedrich, Owen <000968723@mohawkcollege.ca>
2025-11-18 10:44:59 -0500

    Basic camera

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -2,18 +2,12 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
-    Transform FollowTarget;
+    public Transform FollowTarget;
     public Vector3 Offset = new Vector3(0, 5, -10);
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // FixedUpdate is called once per physics update
     void FixedUpdate()
     {
-        
+        transform.position = FollowTarget.position + Offset;
     }
 }
