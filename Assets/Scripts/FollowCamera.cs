bde6eeb85ef5662af44fb54691d9fc49eebfad77
Haedrich, Owen <000968723@mohawkcollege.ca>
2025-11-18 10:44:13 -0500

    Make prefabs of created elements

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    Transform FollowTarget;
+    public Vector3 Offset = new Vector3(0, 5, -10);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // FixedUpdate is called once per physics update
+    void FixedUpdate()
+    {
+        
+    }
+}
